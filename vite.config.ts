ae58a9795e3e63d14a8da7af47c3297c1bcb4b00
FLaNEUR-Q <88171815+FLaNEUR-Q@users.noreply.github.com>
2025-04-16 02:30:50 +0900

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
+  define: {
+    'process.env': {
+      VITE_OPENAI_API_KEY: JSON.stringify(process.env.VITE_OPENAI_API_KEY)
+    }
+  }
+})
