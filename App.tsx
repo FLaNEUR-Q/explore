d05c3cff064abd71e0a72b5177ebea536ece4034
FLaNEUR-Q <88171815+FLaNEUR-Q@users.noreply.github.com>
2025-04-16 22:17:14 +0900

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -4,54 +4,18 @@ import { OrbitControls, Stars } from '@react-three/drei'
 import * as THREE from 'three'
 
 function Earth() {
-  const planetRef = useRef()
-  const cloudRef = useRef()
-  const atmosphereRef = useRef()
-
+  const ref = useRef()
   const texture = new THREE.TextureLoader().load("/2k_earth_daymap.jpg")
-  const nightMap = new THREE.TextureLoader().load("/2k_earth_nightmap.jpg")
-  const bumpMap = new THREE.TextureLoader().load("/2k_earth_bump.jpg")
-  const cloudMap = new THREE.TextureLoader().load("/earth_clouds_1024.png")
 
   useFrame(() => {
-    if (planetRef.current) planetRef.current.rotation.y += 0.0015
-    if (cloudRef.current) cloudRef.current.rotation.y += 0.002
-    if (atmosphereRef.current) atmosphereRef.current.rotation.y += 0.001
+    if (ref.current) ref.current.rotation.y += 0.0015
   })
 
   return (
-    <>
-      <mesh ref={planetRef}>
-        <sphereGeometry args={[2, 64, 64]} />
-        <meshStandardMaterial
-          map={texture}
-          bumpMap={bumpMap}
-          bumpScale={0.03}
-          emissiveMap={nightMap}
-          emissiveIntensity={1.0}
-          metalness={0.3}
-          roughness={1}
-        />
-      </mesh>
-      <mesh ref={cloudRef}>
-        <sphereGeometry args={[2.02, 64, 64]} />
-        <meshStandardMaterial
-          map={cloudMap}
-          transparent
-          opacity={0.35}
-          depthWrite={false}
-        />
-      </mesh>
-      <mesh ref={atmosphereRef}>
-        <sphereGeometry args={[2.15, 64, 64]} />
-        <meshBasicMaterial
-          color="#aadfff"
-          transparent
-          opacity={0.15}
-          side={THREE.BackSide}
-        />
-      </mesh>
-    </>
+    <mesh ref={ref}>
+      <sphereGeometry args={[2, 128, 128]} />
+      <meshStandardMaterial map={texture} />
+    </mesh>
   )
 }
 
@@ -59,7 +23,7 @@ export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas camera={{ position: [0, 0, 7] }}>
-        <ambientLight intensity={0.7} />
+        <ambientLight intensity={0.8} />
         <directionalLight position={[5, 2, 5]} intensity={2} />
         <Stars radius={100} depth={50} count={5000} factor={4} fade speed={1} />
         <Earth />
